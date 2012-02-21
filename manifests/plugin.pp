504e16c8f7ff5350927d168af66ffdb32c923bf2
Philippe Laflamme <philippe.laflamme@gmail.com>
2012-02-21 13:37:40 -0500

    Specify group when running plugin command

--- manifests/plugin.pp
+++ manifests/plugin.pp
@@ -5,6 +5,7 @@ define elasticsearch::plugin($plugin = $name, $source = $name, $cmd="install") {
   exec { "install-plugin-${plugin}":
     command => "/usr/local/elasticsearch/bin/plugin -${cmd} $source",
     user    => "elasticsearch",
+    group   => "elasticsearch",
     creates => "/usr/local/elasticsearch/plugins/$plugin",
     require => File["/usr/local/elasticsearch"],
     notify  => Service["elasticsearch"],
